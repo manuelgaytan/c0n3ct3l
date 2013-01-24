72f258d5e7d9bedf24a747b07ecd256c5feec48f
mgaytanh@gmail.com <mgaytanh@gmail.com@db0811c6-65b1-2544-9fa0-b2e2b4e66998>
2013-01-24 07:01:32 +0000

    Ajsute a Configuracion Prueba y Entrega (agregar campos Entregables Completos, Fecha y Estado de Validacion Operativa).

---
+++ trunk/backend/serviciosEJB/ejbModule/mx/com/gahm/conenctel/entities/EstadoValidacionOperativaDO.java
@@ -0,0 +1,55 @@
+/**
+ * 
+ */
+package mx.com.gahm.conenctel.entities;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+/**
+ * @author Manuel Gayt&aacute;n.
+ * 
+ */
+@Entity
+@Table(name = "EstadoValidacionOperativa")
+@NamedQueries({
+	@NamedQuery(name = "EstadoValidacionOperativaDO.findAll", query = "select es from EstadoValidacionOperativaDO es ")})
+public class EstadoValidacionOperativaDO implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1258999436745740307L;
+
+	@Id
+	@GeneratedValue(strategy = IDENTITY)
+	@Column(name = "id", unique = true, nullable = false)
+	private Integer id;
+	@Column(name = "estado")
+	private String etiqueta;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getEtiqueta() {
+		return etiqueta;
+	}
+
+	public void setEtiqueta(String etiqueta) {
+		this.etiqueta = etiqueta;
+	}
+
+}

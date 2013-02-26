67453102b2de355718d385d4f977eabf5b535497
mgaytanh@gmail.com <mgaytanh@gmail.com@db0811c6-65b1-2544-9fa0-b2e2b4e66998>
2013-02-26 12:06:16 +0000

    ajuste requisicion orden de compra.

--- trunk/backend/serviciosEJB/ejbModule/mx/com/gahm/conenctel/entities/PartidaRequisicionCompraDO.java
+++ trunk/backend/serviciosEJB/ejbModule/mx/com/gahm/conenctel/entities/PartidaRequisicionCompraDO.java
@@ -1,6 +1,7 @@
 package mx.com.gahm.conenctel.entities;
 
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;

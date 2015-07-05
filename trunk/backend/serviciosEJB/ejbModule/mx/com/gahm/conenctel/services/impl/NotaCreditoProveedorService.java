de0f7a84ad2eb5599860e218c94f65f725d0f427
mgaytanh@gmail.com <mgaytanh@gmail.com@db0811c6-65b1-2544-9fa0-b2e2b4e66998>
2015-07-05 21:10:54 +0000

    items de notificaciónes a tesorería

--- trunk/backend/serviciosEJB/ejbModule/mx/com/gahm/conenctel/services/impl/NotaCreditoProveedorService.java
+++ trunk/backend/serviciosEJB/ejbModule/mx/com/gahm/conenctel/services/impl/NotaCreditoProveedorService.java
@@ -1,5 +1,6 @@
 package mx.com.gahm.conenctel.services.impl;
 
+import java.util.Date;
 import java.util.List;
 
 import javax.ejb.Stateless;
@@ -7,8 +8,12 @@ import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
 
+import mx.com.gahm.conenctel.entities.EstadoNotificacionDO;
 import mx.com.gahm.conenctel.entities.FacturaProveedorDO;
 import mx.com.gahm.conenctel.entities.NotaCreditoProveedorDO;
+import mx.com.gahm.conenctel.entities.NotificacionDO;
+import mx.com.gahm.conenctel.entities.PerfilDO;
+import mx.com.gahm.conenctel.entities.UsuarioDO;
 import mx.com.gahm.conenctel.services.INotaCreditoProveedorService;
 
 @Stateless(mappedName = "ejb/NotaCreditoProveedorService")
@@ -45,19 +50,42 @@ public class NotaCreditoProveedorService implements INotaCreditoProveedorService
 	@Override
 	public NotaCreditoProveedorDO save(NotaCreditoProveedorDO item) {
 	    try {
-	    	
-	    	
 	    	entityManager.persist(item);
-	    	
-	    	
+	    	this.validarEnvioNotificaciones(item);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
 	return item;
 	}
 
+	private void validarEnvioNotificaciones(NotaCreditoProveedorDO notaCreditoProveedor) {
+		if( notaCreditoProveedor == null ||
+			(notaCreditoProveedor.getProveedorCalificado() == null &&
+			 notaCreditoProveedor.getProveedorMaquilador() == null) ){
+			return;
+		}
+		String mensaje = null;
+		if( notaCreditoProveedor.getProveedorCalificado() != null ){
+			mensaje = NotificacionDO.NOTA_CREDITO_PROVEEDOR_BIENES_SERVICIOS_TESORERIA + notaCreditoProveedor.getProveedorCalificado().getId();
+		}
+		if( notaCreditoProveedor.getProveedorMaquilador() != null ){
+			mensaje = NotificacionDO.NOTA_CREDITO_PROVEEDOR_MAQUILADO_TESORERIA + notaCreditoProveedor.getProveedorMaquilador().getId();
+		}
+		this.enviarNotificacion(PerfilDO.ID_TESORERIA, mensaje);
+	}
 	
+	private void enviarNotificacion(int idPerfil, String mensaje) {
+		PerfilDO perfil = entityManager.find(PerfilDO.class, idPerfil);
+		UsuarioDO usuario = entityManager.find(UsuarioDO.class, UsuarioDO.ID_AUTOMATICO);
+		EstadoNotificacionDO estado = entityManager.find(EstadoNotificacionDO.class, EstadoNotificacionDO.ID_PENDIENTE);
+		NotificacionDO notificacion = new NotificacionDO();
+		notificacion.setPerfil(perfil);
+		notificacion.setNotificacion(mensaje);
+		notificacion.setUsuarioCreacion(usuario);
+		notificacion.setFechaHoraCreacion(new Date());
+		notificacion.setEstado(estado);
+		entityManager.persist(notificacion);
+	}
 	
 	@Override
 	public NotaCreditoProveedorDO update(NotaCreditoProveedorDO item) {
